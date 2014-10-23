06f060c9bc269b03e4d4d42c852de8f42edb2912
Jacob Stuart <stuart4@purdue.edu>
2014-10-23 19:11:43 -0400

    implements candidate search functionality per cell

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -11,7 +11,19 @@ public class Sudoku {
  private int rows, columns;
 
  public static void main(String[] args) {
-  Sudoku game = new Sudoku();
+  int[][] sudoku = new int[9][9];
+  sudoku[0] = new int[]{2,4,5,      8,7,1,      3,9,6};
+  sudoku[1] = new int[]{1,8,7,      6,3,9,      2,5,4};
+  sudoku[2] = new int[]{9,6,3,      2,4,5,      7,8,1};
+
+  sudoku[3] = new int[]{3,2,4,      9,6,7,      5,1,8};
+  sudoku[4] = new int[]{5,9,8,      1,2,3,      6,4,7};
+  sudoku[5] = new int[]{7,1,6,      5,8,4,      9,2,3};
+
+  sudoku[6] = new int[]{6,7,9,      4,5,8,      1,3,2};
+  sudoku[7] = new int[]{4,5,2,      3,1,6,      8,7,9};
+  sudoku[8] = new int[]{8,3,1,      7,9,2,      4,6,5};
+  Sudoku game = new Sudoku(sudoku);
  }
 
  public Sudoku() {
@@ -33,12 +45,38 @@ public class Sudoku {
    candidates[i] = true;
   }
 
+  int[] house = getHouse(row, column);
+  int[] rowArray = getRow(row);
+  int[] columnArray = getColumn(column);
 
+  for (int i = 0; i < house.length; i++) {
+   if (house[i] != 0) {
+    candidates[house[i] - 1] = false;
+   }
+  }
+
+  for (int i = 0; i < rowArray.length; i++) {
+   if (rowArray[i] != 0) {
+    candidates[rowArray[i] - 1] = false;
+   }
+  }
+
+  for (int i = 0; i < columnArray.length; i++) {
+   if (columnArray[i] != 0) {
+    candidates[columnArray[i] - 1] = false;
+   }
+  }
 
   return candidates;
  }
 
- private int[] getColumn(int columnIndex) {
+ //TODO private
+ public int[] getRow(int rowIndex) {
+  return board[rowIndex];
+ }
+
+//TODO private
+ public int[] getColumn(int columnIndex) {
   int[] column = new int[this.columns];
   for (int i = 0; i < column.length; i++) {
    column[i] = board[i][columnIndex];
@@ -46,16 +84,17 @@ public class Sudoku {
 
   return column;
  }
-
- private int[] getHouse(int row, int column) {
+//TODO private
+ public int[] getHouse(int row, int column) {
   row = row / 3 * 3;
   column = column / 3 * 3;
   int[] house = new int[9];
   for (int i = 0; i < 3; i++) {
    for (int k = 0; k < 3; k++) {
-    house[i + k] = board[i][k];
+    house[i + k * 3] = board[i + row][k + column];
    }
   }
+  return house;
  }
 
  public boolean isSolved() {
