3e17e686f60f017e97c021ef96f690c3958e119d
Jacob Stuart <stuart4@purdue.edu>
2014-10-28 14:49:28 -0400

    105% - feature complete

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,207 +1,206 @@
 public class Sudoku {
 
-	int[][] board;
+ int[][] board;
 
-	public Sudoku() {
-		board = new int[9][9];
-	}
+ public Sudoku() {
+  board = new int[9][9];
+ }
 
-	//creates a Sudoku with an initial board
-	public Sudoku(int[][] board) {
-		this.board = clone(board);
-	}
+ //creates a Sudoku with an initial board
+ public Sudoku(int[][] board) {
+  this.board = clone(board);
+ }
 
-	public int[][] clone(int[][] input) {
-		int[][] output = new int[input.length][input[0].length];
-		for (int row = 0; row < output.length; row++) {
-			for (int col = 0; col < output[row].length; col++) {
-				output[row][col] = input[row][col];
-			}
-		}
-		return output;
-	}
+ public int[][] clone(int[][] input) {
+  int[][] output = new int[input.length][input[0].length];
+  for (int row = 0; row < output.length; row++) {
+   for (int col = 0; col < output[row].length; col++) {
+    output[row][col] = input[row][col];
+   }
+  }
+  return output;
+ }
 
-	//returns a copy of the current state of the board
-	public int[][] board() {
-		return clone(board);
-	}
+ //returns a copy of the current state of the board
+ public int[][] board() {
+  return clone(board);
+ }
 
-	//returns list of candidates for a specific cell. Array contains
-	//true at index i if i is a candidate for the cell at row and column,
-	//so long as cell is not already set, in which case there are none
-	public boolean[] candidates(int row, int column) {
-		boolean[] candidates = new boolean[10];
-		for (int i = 1; i < candidates.length; i++) {
-			candidates[i] = true;
-		}
+ //returns list of candidates for a specific cell. Array contains
+ //true at index i if i is a candidate for the cell at row and column,
+ //so long as cell is not already set, in which case there are none
+ public boolean[] candidates(int row, int column) {
+  boolean[] candidates = new boolean[10];
+  for (int i = 1; i < candidates.length; i++) {
+   candidates[i] = true;
+  }
 
-		int houseRow = row / 3 * 3;
-		int houseColumn = column / 3 * 3;
+  int houseRow = row / 3 * 3;
+  int houseColumn = column / 3 * 3;
 
-		for (int i = houseRow; i < 3 + houseRow; i++) {
-			for (int j = houseColumn; j < 3 + houseColumn; j++) {
-				if (board[i][j] != 0) {
-					candidates[board[i][j]] = false;
-				}
-			}
-		}
+  for (int i = houseRow; i < 3 + houseRow; i++) {
+   for (int j = houseColumn; j < 3 + houseColumn; j++) {
+    if (board[i][j] != 0) {
+     candidates[board[i][j]] = false;
+    }
+   }
+  }
 
-		for (int i = 0; i < board[row].length; i++) {
-			if (board[row][i] != 0) {
-				candidates[board[row][i]] = false;
-			}
-		}
+  for (int i = 0; i < board[row].length; i++) {
+   if (board[row][i] != 0) {
+    candidates[board[row][i]] = false;
+   }
+  }
 
-		for (int i = 0; i < board.length; i++) {
-			if (board[i][column] != 0) {
-				candidates[board[i][column]] = false;
-			}
-		}
-		return candidates;
-	}
+  for (int i = 0; i < board.length; i++) {
+   if (board[i][column] != 0) {
+    candidates[board[i][column]] = false;
+   }
+  }
+  return candidates;
+ }
 
-	//returns true if board is solved
-	public boolean isSolved() {
-		boolean isSolved = true;
-		for (int row = 0; row < 9; row++) {
-			boolean[] nums = new boolean[10];
-			for (int col = 0; col < 9; col++) {
-				nums[board[row][col]] = true;
-			}
-			for (int i = 1; i < nums.length; i++) {
-				if (!nums[i]) {
-					isSolved = false;
-				}
-			}
-		}
-		for (int row = 0; row < 9; row++) {
-			boolean[] nums = new boolean[10];
-			for (int col = 0; col < 9; col++) {
-				nums[board[col][row]] = true;
-			}
-			for (int i = 1; i < nums.length; i++) {
-				if (!nums[i]) {
-					isSolved = false;
-				}
-			}
-		}
-
-
-		for (int houseRow = 0; houseRow < 9; houseRow += 3) {
-			for (int houseCol = 0; houseCol < 9; houseCol += 3) {
-				boolean[] nums = new boolean[10];
-				for (int row = 0; row < 3; row++) {
-					for (int col = 0; col < 3; col++) {
-						nums[board[row + houseRow][col + houseCol]] = true;
-					}
-				}
-				for (int i = 1; i < nums.length; i++) {
-					if (!nums[i]) {
-						isSolved = false;
-					}
-				}
-			}
-		}
+ //returns true if board is solved
+ public boolean isSolved() {
+  boolean isSolved = true;
+  for (int row = 0; row < 9; row++) {
+   boolean[] nums = new boolean[10];
+   for (int col = 0; col < 9; col++) {
+    nums[board[row][col]] = true;
+   }
+   for (int i = 1; i < nums.length; i++) {
+    if (!nums[i]) {
+     isSolved = false;
+    }
+   }
+  }
+  for (int row = 0; row < 9; row++) {
+   boolean[] nums = new boolean[10];
+   for (int col = 0; col < 9; col++) {
+    nums[board[col][row]] = true;
+   }
+   for (int i = 1; i < nums.length; i++) {
+    if (!nums[i]) {
+     isSolved = false;
+    }
+   }
+  }
 
 
-		return isSolved;
-	}
+  for (int houseRow = 0; houseRow < 9; houseRow += 3) {
+   for (int houseCol = 0; houseCol < 9; houseCol += 3) {
+    boolean[] nums = new boolean[10];
+    for (int row = 0; row < 3; row++) {
+     for (int col = 0; col < 3; col++) {
+      nums[board[row + houseRow][col + houseCol]] = true;
+     }
+    }
+    for (int i = 1; i < nums.length; i++) {
+     if (!nums[i]) {
+      isSolved = false;
+     }
+    }
+   }
+  }
 
-	public boolean nakedSingles() {
 
-		boolean changesMade = false;
+  return isSolved;
+ }
 
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				boolean[] result = candidates(i, j);
-				int possibleCandidates = 0;
-				for (int k = 1; k < result.length; k++) {
-					if (result[k]) {
-						possibleCandidates++;
-					}
-				}
-				if (possibleCandidates == 1 && board[i][j] == 0) {
-					for (int k = 1; k <= 9; k++) {
-						if (result[k]) {
-							board[i][j] = k;
-							changesMade = true;
-						}
-					}
-				}
-			}
-		}
-		return changesMade;
-	}
+ public boolean nakedSingles() {
 
-	public boolean hiddenSingles() {
+  boolean changesMade = false;
 
-		boolean changesMade = false;
+  for (int i = 0; i < 9; i++) {
+   for (int j = 0; j < 9; j++) {
+    boolean[] result = candidates(i, j);
+    int possibleCandidates = 0;
+    for (int k = 1; k < result.length; k++) {
+     if (result[k]) {
+      possibleCandidates++;
+     }
+    }
+    if (possibleCandidates == 1 && board[i][j] == 0) {
+     for (int k = 1; k <= 9; k++) {
+      if (result[k]) {
+       board[i][j] = k;
+       changesMade = true;
+      }
+     }
+    }
+   }
+  }
+  return changesMade;
+ }
 
-		for (int number = 0; number <= 9; number++) {
-			for (int row = 0; row < 9; row++) {
-				int timesFound = 0;
-				int lastRow = -1;
-				int lastColumn = -1;
-				for (int column = 0; column < 9; column++) {
-					if (board[row][column] == number || (board[row][column] == 0 && candidates(row, column)[number])) {
-						timesFound++;
-						lastRow = row;
-						lastColumn = column;
-					}
-				}
+ public boolean hiddenSingles() {
 
-				if (timesFound == 1 && board[lastRow][lastColumn] == 0) {
-					changesMade = true;
-					board[lastRow][lastColumn] = number;
-				}
-			}
+  boolean changesMade = false;
 
-			for (int col = 0; col < 9; col++) {
-				int timesFound = 0;
-				int lastRow = -1;
-				int lastColumn = -1;
-				for (int row = 0; row < 9; row++) {
-					if (board[row][col] == number || (board[row][col] == 0 && candidates(row, col)[number])) {
-						timesFound++;
-						lastRow = row;
-						lastColumn = col;
-					}
-				}
+  for (int number = 1; number <= 9; number++) {
+   for (int row = 0; row < 9; row++) {
+    int timesFound = 0;
+    int lastRow = -1;
+    int lastColumn = -1;
+    for (int column = 0; column < 9; column++) {
+     if (board[row][column] == number || (board[row][column] == 0 && candidates(row, column)[number])) {
+      timesFound++;
+      lastRow = row;
+      lastColumn = column;
+     }
+    }
 
-				if (timesFound == 1 && board[lastRow][lastColumn] == 0) {
-					changesMade = true;
-					board[lastRow][lastColumn] = number;
-				}
-			}
-			for (int houseRow = 0; houseRow < 9; houseRow += 3) {
-				for (int houseCol = 0; houseCol < 9; houseCol += 3) {
-					int timesFound = 0;
-					int lastRow = -1;
-					int lastColumn = -1;
-					for (int row = 0; row < 3; row++) {
-						for (int col = 0; col < 3; col++) {
-							if (board[row + houseRow][col + houseCol] == number
-								|| (board[row + houseRow][col + houseCol] == 0 &&
-								candidates(row + houseRow, col + houseCol)[number])) {
-								timesFound++;
-								lastRow = row + houseRow;
-								lastColumn = col + houseCol;
-							}
-						}
-					}
-					if (timesFound == 1 && board[lastRow][lastColumn] == 0) {
-						changesMade = true;
-						board[lastRow][lastColumn] = number;
-					}
-				}
-			}
-		}
-		return changesMade;
-	}
+    if (timesFound == 1 && board[lastRow][lastColumn] == 0) {
+     changesMade = true;
+     board[lastRow][lastColumn] = number;
+    }
+   }
 
-	//attempts to solve board. exits when solved or no updates made
-	public void solve() {
-		while (!isSolved() && (nakedSingles() || hiddenSingles())) ;
-	}
+   for (int col = 0; col < 9; col++) {
+    int timesFound = 0;
+    int lastRow = -1;
+    int lastColumn = -1;
+    for (int row = 0; row < 9; row++) {
+     if (board[row][col] == number || (board[row][col] == 0 && candidates(row, col)[number])) {
+      timesFound++;
+      lastRow = row;
+      lastColumn = col;
+     }
+    }
 
+    if (timesFound == 1 && board[lastRow][lastColumn] == 0) {
+     changesMade = true;
+     board[lastRow][lastColumn] = number;
+    }
+   }
+   for (int houseRow = 0; houseRow < 9; houseRow += 3) {
+    for (int houseCol = 0; houseCol < 9; houseCol += 3) {
+     int timesFound = 0;
+     int lastRow = -1;
+     int lastColumn = -1;
+     for (int row = 0; row < 3; row++) {
+      for (int col = 0; col < 3; col++) {
+       if (board[row + houseRow][col + houseCol] == number
+        || (board[row + houseRow][col + houseCol] == 0 &&
+        candidates(row + houseRow, col + houseCol)[number])) {
+        timesFound++;
+        lastRow = row + houseRow;
+        lastColumn = col + houseCol;
+       }
+      }
+     }
+     if (timesFound == 1 && board[lastRow][lastColumn] == 0) {
+      changesMade = true;
+      board[lastRow][lastColumn] = number;
+     }
+    }
+   }
+  }
+  return changesMade;
+ }
+
+ //attempts to solve board. exits when solved or no updates made
+ public void solve() {
+  while (!isSolved() && (nakedSingles() || hiddenSingles())) ;
+ }
 }
